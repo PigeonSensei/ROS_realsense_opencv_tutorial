090693a9c7ae7e900890b74e5b3a2582f62d019d
PigeonSensei <parkour5813@gmail.com>
2020-10-15 18:12:11 +0900

    -

--- src/ros_realsense_opencv_tutorial.cpp
+++ src/ros_realsense_opencv_tutorial.cpp
@@ -6,39 +6,46 @@
 using namespace cv;
 using namespace std;
 
+void test()
+{
+  cv::Mat test(640,480,CV_8UC3);
+  imshow("aaa", test);
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "ros_realsense_opencv_tutorial");
   ros::NodeHandle nh;
 
-  cout << "OpenCV version : " << CV_VERSION << endl;
-  cout << "Major version : "  << CV_MAJOR_VERSION << endl;
+//  cout << "OpenCV version : " << CV_VERSION << endl;
+//  cout << "Major version : "  << CV_MAJOR_VERSION << endl;
 
-  rs2::pipeline pipe;
-  rs2::config cfg;
+  rs2::pipeline pipe; //
+  rs2::config cfg; //
+  rs2::frameset frames; //
+  rs2::frame color_frame;
+
+
   cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
   pipe.start(cfg);
-
-  rs2::frameset frames;
 
   for(int i=0; i < 30; i ++)
   {
     frames = pipe.wait_for_frames();
   }
 
-  ROS_INFO("Hello world!");
-
-  namedWindow("Display Image", WINDOW_AUTOSIZE);
+  namedWindow("Display Imagee", WINDOW_AUTOSIZE);
 
   ros::Rate loop_rate(30);
 
   while(ros::ok())
   {
     frames = pipe.wait_for_frames();
-    rs2::frame color_frame = frames.get_color_frame();
+    color_frame = frames.get_color_frame();
     Mat color(Size(640,480), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
 
     imshow("Display Image", color);
+    test();
 
     if(waitKey(10)==27) break;
     loop_rate.sleep();
